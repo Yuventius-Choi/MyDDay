0d227fd4190bef7f1aa136ba633db922e03460a4
VenChoi <yuventius125@github.com>
2026-04-13 17:05:00 +0900

    Initial Commit

---
+++ MyDDayTests/MyDDayTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyDDayTests.swift
+//  MyDDayTests
+//
+//  Created by Yuventius Choi on 4/13/26.
+//
+
+import Testing
+@testable import MyDDay
+
+struct MyDDayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
