552c043f44377503bc96e21d1295f312ca83e17c
VenChoi <yuventius125@github.com>
2026-04-15 16:48:58 +0900

    Data, Domain, Presenter updated

---
+++ MyDDay/Presentation/UI/Home/HomeVM.swift
@@ -0,0 +1,49 @@
+//
+//  HomeVM.swift
+//  MyDDay
+//
+//  Created by Yuventius Choi on 4/15/26.
+//
+
+import Combine
+
+final class HomeVM: ObservableObject {
+    private let useCase: FetchDDaysUseCase
+    
+    @Published
+    private(set) var state = State()
+    
+    init(useCase: FetchDDaysUseCase) {
+        self.useCase = useCase
+    }
+    
+    
+    func send(_ action: Action) {
+        switch action {
+        case .onAppear:
+        case .refresh:
+        case .loaded(let items):
+            state.isLoading = false
+            state.dDays = items
+        }
+    }
+    
+    func fetch() async {
+        do {
+            try await send(.loaded(useCase.execute()))
+        } catch {
+            print(error)
+        }
+    }
+    
+    struct State {
+        var isLoading: Bool = true
+        var dDays: [DDay] = []
+    }
+    
+    enum Action {
+        case onAppear
+        case refresh
+        case loaded([DDay])
+    }
+}
